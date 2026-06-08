9d0e0fd404f2111d29e58f9c30f6357c40386c5b
Ömer Ulucan <omer.ulucan80@gmail.com>
2026-06-08 17:33:44 -0500

    added User model

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from db.database import Base
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class User(Base):
+    __tablename__ = "users"
+    
+    uuid = Column(UUID(as_uuid=True), unique=True, primary_key=True, default=uuid.uuid4)
+    username = Column(String, unique=True)
+    password = Column(String)
+    created_at = Column(DateTime)
