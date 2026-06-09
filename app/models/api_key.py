63fefdd3ef9e6e70c1310235b432b9e2e5d566a3
Ömer Ulucan <omer.ulucan80@gmail.com>
2026-06-08 19:49:25 -0500

    added APIKey model

---
+++ app/models/api_key.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from db.database import Base
+import uuid
+
+
+class API_Key(Base):
+    __tablename__ = "api-key"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_uuid = Column(UUID(as_uuid=True), ForeignKey("users.uuid"), nullable=False)
+    api_key = Column(String(), unique=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime)
